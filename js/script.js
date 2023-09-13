e474c7bc499e524dcda4350e45a163f0dacdb615
João Vitor Nunes da Silva <135175090+Joao-Vitor-Nunes@users.noreply.github.com>
2023-09-13 15:14:47 -0400

    att-completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 const jump = () => {
-    mario.classList.add('jump')
+    mario.classList.add('jump') /*adicionada a class jump no mario*/ 
+
     setTimeout(() => {
-        mario.classList.remove('jump')
+        mario.classList.remove('jump') /*remove a class jump, (para dar reset no pulo) */
     }, 500)
+
 }
 
-document.addEventListener('keydown', jump)
+
+const loop = setInterval(() => {
+
+    const pipe_position = pipe.offsetLeft;
+    const mario_position = +window.getComputedStyle(mario).bottom.replace('px', '')
+    console.log(mario_position)
+
+    
+
+    if (pipe_position <= 145 && mario_position < 105 && pipe_position >  0) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipe_position}px `
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${mario_position}px`
+
+        mario.src = '/imagens/game-over.png'
+        mario.style.width = '80px'
+        mario.style.marginLeft = '70px'
+
+        clearInterval(loop)
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump) /*quando pressionar a tecla*/
